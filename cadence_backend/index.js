7516c34e9ed50b42dfcc8ab657a79812259ec415
Saurabhpl2408 <saurabhpl14@gmail.com>
2023-12-07 00:44:50 +0530

    feat: Connection to db

--- cadence_backend/index.js
+++ cadence_backend/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app =express();
 const port = 8000;
 
+mongoose.connect("mongodb+srv://admin:Wx2vIJGlm9aQQa1S@cluster0.w2flgrw.mongodb.net/?retryWrites=true&w=majority",
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    }
+).then((x)=>{
+    console.log("Connected to db")
+}).catch((err)=> {
+    console.log(err);
+});
+
+// const connectDB = async ()=>{
+//     try{
+//         const conn = await mongoose.connect()
+//     }
+// }
 app.get('/', (req,res)=>{
     res.send("HW!!!!");
 });
