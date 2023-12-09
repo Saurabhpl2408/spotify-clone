ccf5a1ca8ebd8fbc20a9372dc4d6594d0ae4678a
Saurabhpl2408 <saurabhpl14@gmail.com>
2023-12-09 23:27:21 +0530

    feat:: Adding JWT boilerplate

--- cadence_backend/index.js
+++ cadence_backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+var JwtStrategy = require('passport-jwt').Strategy,
+ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport =require("passport");
+const User = require("./models/user")
 require('dotenv').config();
 const app =express();
 const port = 8000;
@@ -14,6 +18,25 @@ mongoose.connect("mongodb+srv://admin:Wx2vIJGlm9aQQa1S@cluster0.w2flgrw.mongodb.
 }).catch((err)=> {
     console.log(err);
 });
+
+
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secret';
+opts.issuer = 'accounts.examplesoft.com';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
 
 // const connectDB = async ()=>{
 //     try{
