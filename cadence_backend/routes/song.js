ea041d9dd6c60c4f53e1e859f33fccc85db8b6a4
Saurabhpl2408 <saurabhpl14@gmail.com>
2023-12-14 22:48:05 +0530

    feat: songs route

---
+++ cadence_backend/routes/song.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const passport = require("passport");
+const Song = require("./../models/song");
+router.post('/create', passport.authenticate("jwt", {session: false}), async (req,res) => {
+    const{name, thumbnail, track} = req.body;
+    if(!name || !thumbnail || !track){
+        return res.status(301).json({err:"Insufficient details of the song"});
+    };
+    const artist = req.user._id;
+    const songDetails = {name, thumbnail, track, artist};
+    const createdSong = await Song.create(songDetails);
+    return res.status(200).json({createdSong});
+});
+
+router.get('/get/mysongs', passport.authenticate("jwt", {session: false}), async (req,res)=>{
+    const currentUser = req.user;
+    const songs = await Song.find({artist: req.user._id});
+    return res.status(200).json({data: songs});
+})
+module.exports = router;
